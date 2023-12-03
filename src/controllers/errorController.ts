9548af3c5dba3fd906c2b7bd5d5f0a919a4fcf17
bilalshahid929 <bilalshahid929@gmail.com>
2023-12-03 23:49:58 +0400

    add the globalErrorHandler

---
+++ src/controllers/errorController.ts
@@ -0,0 +1,21 @@
+import { Request, Response, NextFunction } from "express";
+
+interface AppError extends Error {
+  statusCode?: number;
+  status?: string;
+}
+
+export const globalErrorHandler = (
+  err: AppError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
