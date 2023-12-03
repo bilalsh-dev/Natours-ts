aacc42908bd20c72cd39150f4def32a7ebe8bda1
bilalshahid929 <bilalshahid929@gmail.com>
2023-12-03 23:35:17 +0400

    add the catch all error

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
-import express, { Application, NextFunction, Request, Response } from 'express';
-import morgan from 'morgan';
-import tourRouter from './routes/tourRoutes';
-import userRouter from './routes/userRoutes';
+import express, { Application, NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import tourRouter from "./routes/tourRoutes";
+import userRouter from "./routes/userRoutes";
+import AppError from "./utils/appError";
 const app: Application = express();
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.static(`${__dirname}/../public`));
 app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log('Hello from middleWare');
+  console.log("Hello from middleWare");
   next();
 });
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
 
 export default app;
